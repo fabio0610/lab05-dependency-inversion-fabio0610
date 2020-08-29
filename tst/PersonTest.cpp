26d4f805cbd4b069887fa81341d86cd41ff3971f
fabio0610 <fabio0610@hotmail.com>
2020-08-29 17:44:33 -0600

    changes in PersonTest

--- tst/PersonTest.cpp
+++ tst/PersonTest.cpp
@@ -2,6 +2,7 @@
 // Created by Maikol Guzman on 8/24/20.
 //
 
+
 #include <BankTransferSender.h>
 #include <Person.h>
 #include <CashSender.h>
@@ -9,9 +10,13 @@
 #include "gtest/gtest.h"
 
 TEST(PersonTestSuite, VerifyProcessPayment){
+    IBankServices *bankTransferSender = new BankTransferSender();
+    IBankServices *cashSender = new CashSender();
+    IBankServices *checkSender = new CheckSender();
+
     Person person;
 
-    EXPECT_EQ(person.processPaymentBankTransfer(), "Sending the money by transference");
-    EXPECT_EQ(person.processPaymentCash(), "Give the money in the hands");
-    EXPECT_EQ(person.processPaymentCheck(), "Sending the check with the money");
+    EXPECT_EQ(person.payment(bankTransferSender), "Sending the money by transference");
+    EXPECT_EQ(person.payment(cashSender), "Give the money in the hands");
+    EXPECT_EQ(person.payment(checkSender), "Sending the check with the money");
 }
